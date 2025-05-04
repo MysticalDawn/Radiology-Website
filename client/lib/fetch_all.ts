975aafd39cb460f8cd4e99b58cecacbc9e6325d3
Abdulwahab <wahab210899@gmail.com>
2025-05-04 05:20:22 +0300

    some

--- client/lib/fetch_all.ts
+++ client/lib/fetch_all.ts
@@ -1,7 +1,7 @@
 export async function getAllPatients() {
   try {
     const response = await fetch(
-      `https://radiology-website-server-d82ugcbtz-mysticaldawns-projects.vercel.app/fetch_all`,
+      `https://radiology-website-server-gshzkqdzu-mysticaldawns-projects.vercel.app/fetch_all`,
       {
         method: "GET",
       }
