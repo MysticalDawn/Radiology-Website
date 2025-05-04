c50937201e64710ad73046aa921d5254f38dd8af
Abdulwahab <wahab210899@gmail.com>
2025-05-04 03:47:58 +0300

    very minor update

--- client/lib/fetch_all.ts
+++ client/lib/fetch_all.ts
@@ -1,12 +1,17 @@
 export async function getAllPatients() {
   try {
-    const response = await fetch(`http://localhost:3001/fetch_all`, {
-      method: "GET"
-    });
+    const response = await fetch(
+      `https://radiology-website-server-p9dxupytf-mysticaldawns-projects.vercel.app/fetch_all`,
+      {
+        method: "GET",
+      }
+    );
 
     if (!response.ok) {
       const errorDetails = await response.text();
-      throw new Error(`Failed to fetch data: ${response.status} - ${errorDetails}`);
+      throw new Error(
+        `Failed to fetch data: ${response.status} - ${errorDetails}`
+      );
     }
 
     const data = await response.json(); // Await the parsed JSON
